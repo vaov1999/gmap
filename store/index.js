f319b6a4e11f04aa97a769b4e18ac84d919d5682
vaov1999 <vaov1999@gmail.com>
2022-01-18 14:35:19 +0200

    zone added

--- store/index.js
+++ store/index.js
@@ -1,4 +1,6 @@
 import { Loader } from '@googlemaps/js-api-loader'
+import { MarkerClusterer } from '@googlemaps/markerclusterer'
+
 import {
   categoriesOrganization,
   googleMapThemeDark,
@@ -64,7 +66,7 @@ export default {
         .then((google) => {
           const map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: 35.2067563, lng: -111.6066842 },
-            zoom: 9,
+            zoom: 12,
             styles: state.interface.googleMapThemeLight,
           })
           return { google, map }
@@ -90,7 +92,41 @@ export default {
             markers.push(marker)
           })
 
-          return { google, map, markers }
+          // console.log(map)
+          // console.log(mapData)
+          // map.data.addGeoJson(mapData)
+          //
+          // map.data.addListener('click', function (event) {
+          //   map.data.overrideStyle(event.feature, { fillColor: 'red' })
+          // })
+
+          const flightPlanCoordinates = [
+            { lat: 35.2280802, lng: -111.6553596 },
+            { lat: 35.2022815, lng: -111.6668546 },
+            { lat: 35.1933061, lng: -111.6961926 },
+            { lat: 35.1724065, lng: -111.6910456 },
+            { lat: 35.1481337, lng: -111.6795506 },
+            { lat: 35.1505192, lng: -111.650041 },
+            { lat: 35.178719, lng: -111.6466096 },
+            { lat: 35.1872753, lng: -111.6316833 },
+            { lat: 35.2034034, lng: -111.632198 },
+            { lat: 35.2181263, lng: -111.6352862 },
+            { lat: 35.2280802, lng: -111.6553596 },
+          ]
+
+          const flightPath = new google.maps.Polyline({
+            clickable: false,
+            path: flightPlanCoordinates,
+          })
+          map.data.setStyle({
+            strokeOpacity: 0.5,
+            fillColor: 'green',
+            strokeWeight: 3,
+          })
+          flightPath.setMap(map)
+
+          const clusters = new MarkerClusterer({ markers, map })
+          return { google, map, markers, clusters }
         })
     },
     setAppTheme(state) {
